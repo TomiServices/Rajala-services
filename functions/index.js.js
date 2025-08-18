5622f727b0ac76a5d80624cb3deb20ef5dd807d6
TomiServices <Tomi@rajala-services.com>
2025-08-18 22:54:19 +0300

    Update index.js.js

--- functions/index.js.js
+++ functions/index.js.js
@@ -10,8 +10,8 @@ exports.book = functions.https.onRequest((req, res) => {
         if (req.method !== "POST") {
             return res.status(405).json({ error: "Method not allowed" });
         }
-        const { name, email, phone, aika, palvelu, palvelunTyyppi } = req.body;
-        if (!name || !email || !phone || !aika || !palvelu || !palvelunTyyppi) {
+        const { name, email, phone, aika } = req.body;
+        if (!name || !email || !phone || !aika) {
             return res.status(400).json({ error: "Missing required fields" });
         }
         // Tallenna varaus Firestoreen
@@ -20,8 +20,6 @@ exports.book = functions.https.onRequest((req, res) => {
             email,
             phone,
             aika,
-            palvelu,
-            palvelunTyyppi,
             timestamp: admin.firestore.FieldValue.serverTimestamp()
         }).then(doc => {
             // Luo "mail"-dokumentti sähköpostitriggerille (to array!)
@@ -29,8 +27,8 @@ exports.book = functions.https.onRequest((req, res) => {
                 to: [email], // ARRAY, extension vaatii tätä!
                 message: {
                     subject: "Varausvahvistus – Fixnero",
-                    text: `Hei ${name},\n\nVarauksesi ajalle ${aika} palveluun ${palvelu} (${palvelunTyyppi}) on vahvistettu! Kiitos varauksestasi ja tervetuloa.\n\nYhteystietomme:\nPuhelin: 040 1935001\nSähköposti: info@fixnero.fi\nOsoite: Tiilenvalajantie 6, 02330 Espoo\nAukioloajat: Arkisin 9:00-17:00, viikonloppuisin suljettu`,
-                    html: `<strong>Hei ${name},</strong><br><br>Varauksesi ajalle <b>${aika}</b> palveluun <b>${palvelu}</b> (<i>${palvelunTyyppi}</i>) on vahvistettu!<br><br>Kiitos varauksestasi ja tervetuloa.<br><br><strong>Yhteystietomme:</strong><br>Puhelin: 040 1935001<br>Sähköposti: <a href="mailto:info@fixnero.fi">info@fixnero.fi</a><br>Osoite: Tiilenvalajantie 6, 02330 Espoo<br>Aukioloajat: Arkisin 9:00-17:00, viikonloppuisin suljettu`
+                    text: `Hei ${name}, varauksesi ajalle ${aika} on vahvistettu! Kiitos varauksestasi.`,
+                    html: `<strong>Hei ${name},</strong><br>Varauksesi ajalle <b>${aika}</b> on vahvistettu!<br>Kiitos varauksestasi.`
                 }
             }).then(() => {
                 res.json({ success: true, id: doc.id });
